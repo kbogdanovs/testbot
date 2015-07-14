42145f7e8303839bb6c33f806aa9ad15403a238a
kbogdanovs <kalebogdanovs@Kales-MacBook-Air.local>
2015-07-14 18:06:06 -0400

    test

--- hellobot.js
+++ hellobot.js
@@ -1,7 +1,31 @@
-module.exports = function (req, res, next) {
+
+function translate(req)
+{
+var newScript = document.createElement('script');
+ 
+newScript.type = 'text/javascript';
+ 
+//Restful style GET calling
+var source = 'https://www.googleapis.com/language/trnslate/v2?key=AIzaSyB15tsSZiJ4RlEry2QMdbWZEMNZGLgg6wg&q='+ req.body.text +'&target=es&callback=translateText'
+ 
+newScript.src = source;
+ 
+//Adding  dynamic script to head
+dcoument.getElementByTagName('head')[0].appendChild(newScript); //request will be sent
+ 
+}
+ 
+//callback function
+function translateText(response)
+{
+var resultText = response.data.translations[0].trnslatedText;
+
+
+module.exports = function (req, res, resultText next) {
   var userName = req.body.user_name;
+  var transText = 
   var botPayload = {
-    text : 'Hello, ' + userName + '!'
+    text : + resultText +
   };
  
   // avoid infinite loop
